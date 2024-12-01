988faf4b2c92a69114d6dcdd3f7f58ad15bd5867
Maira <mairamasroor05@gmail.com>
2024-11-30 21:29:50 -0500

    Redoing points calculation to remove more points if u use more hints.

--- src/main/java/entity/PointsCalculator.java
+++ src/main/java/entity/PointsCalculator.java
@@ -2,10 +2,9 @@ package entity;
 
 public class PointsCalculator {
     public static final int MAX_SCORE = 800;
-    public static final int HINTS_COST1 = 30;
-    public static final int HINTS_COST2 = 50;
-    public static final int HINTS_COST3 = 70;
+    public static final int HINTS_COST = 2;
     public static final int DISTANCE_DIVIDER = 40;
+    public static final int TIME_MULTIPLIER = 2;
 
 
     public PointsCalculator() {
