5c0b633988a7dc75a09de083788edd08f3be3994
Maira <mairamasroor05@gmail.com>
2024-11-30 20:15:25 -0500

    map sizing

--- src/main/java/entity/Map.java
+++ src/main/java/entity/Map.java
@@ -85,10 +85,12 @@ public class Map extends Application {
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Street View Map with JxBrowser");
-        primaryStage.setFullScreen(true);
+        primaryStage.setWidth(1600);
+        primaryStage.setHeight(1200);
         primaryStage.show();
 
         primaryStage.setOnCloseRequest(event -> engine.close());
+
     }
 
     @JsAccessible
